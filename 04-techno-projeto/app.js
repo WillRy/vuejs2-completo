f4527d77e8cc15bde58cd9214228b501b4d9dd58
William R <wiribasczky@gmail.com>
2021-05-09 15:19:40 -0300

    Modal de detalhes de produto
    - Criada modal para ver detalhes do produto

--- 04-techno-projeto/app.js
+++ 04-techno-projeto/app.js
@@ -2,11 +2,15 @@ const vue = new Vue({
   el: "#app",
   data: {
     produtos: [],
+    produto: false,
   },
-  filters:{
-    numeroPreco(valor){
-      return (+valor).toLocaleString('pt-BR',{style:'currency', currency:'BRL'});
-    }
+  filters: {
+    numeroPreco(valor) {
+      return (+valor).toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      });
+    },
   },
   methods: {
     fetchProdutos() {
@@ -16,8 +20,25 @@ const vue = new Vue({
           this.produtos = dados;
         });
     },
+    fetchProduto(id) {
+      fetch(`./api/produtos/${id}/dados.json`)
+        .then((r) => r.json())
+        .then((dados) => {
+          this.produto = dados;
+        });
+    },
+    abrirModal(id) {
+      this.fetchProduto(id);
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    },
+    fecharModal({ target, currentTarget }) {
+      if (target === currentTarget) this.produto = false;
+    },
   },
-  created(){
+  created() {
     this.fetchProdutos();
-  }
+  },
 });
