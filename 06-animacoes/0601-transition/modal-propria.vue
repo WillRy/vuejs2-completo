436afb0ed4cd8b29b3038276cc77651d0287dec6
William R <wiribasczky@gmail.com>
2021-05-15 17:59:27 -0300

    Aula 0601 transition

---
+++ 06-animacoes/0601-transition/modal-propria.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <button @click="modalAtiva = !modalAtiva">Abrir modal</button>
+    <transition name="modal">
+      <div class="modal-container" v-if="modalAtiva" @click.self="modalAtiva = false">
+        <div class="modal">
+          <p>teste</p>
+        </div>
+      </div>
+    </transition>
+
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      modalAtiva: false
+    }
+  }
+}
+</script>
+<style>
+*{
+  box-sizing: border-box;
+}
+
+.modal-container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
+  background: rgba(0, 0, 0, 0.5);
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  overflow-y: scroll;
+}
+
+.modal {
+  background: #fff;
+  max-width: 650px;
+  padding: 20px;
+  width: 650px;
+  margin: 120px;
+
+  height: 650px;
+}
+
+.modal-enter, .modal-leave-to {
+  opacity: 0;
+}
+
+.modal-enter-active, .modal-leave-active {
+  transition: opacity .3s;
+}
+
+.modal-enter-to .modal {
+  animation: fadeInDown 0.3s;
+}
+
+.modal-leave-to .modal {
+  animation: fadeInDown 0.3s reverse;
+}
+
+@keyframes fadeInDown {
+  from {
+    transform: translate3d(0, -40px, 0);
+  }
+  to {
+    transform: translate3d(0, 0px, 0);
+  }
+}
+
+</style>
---
+++ 06-animacoes/0601-transition/transition.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <button @click="ativo = !ativo">Toggle</button>
+    <transition name="fade">
+      <h1 v-if="ativo">Serviços</h1>  
+    </transition>
+    
+    <transition
+      enter-active-class="animar"
+      leave-active-class="animar"
+      enter-class="animou"
+      leave-to-class="animou"
+    >
+      <h1 v-if="ativo">Serviços</h1>
+    </transition>
+
+    <transition
+      name="fade"
+      @before-enter="beforeEnter"
+      @enter="enter"
+    >
+      <h1 v-if="ativo">Serviços</h1>
+    </transition>
+
+    <transition appear name="fade">
+      <h1>Serviços</h1>
+    </transition>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+      ativo: false
+    }
+  },
+  methods:{
+    enter(elemento){
+      console.log(elemento);
+    },
+    beforeEnter(elemento){
+      console.log(elemento);
+    }
+  }
+}
+</script>
+<style>
+.fade-enter,.fade-leave-to{
+  opacity: 0;
+}
+.fade-enter-active, .fade-leave-active{
+  transition: opacity 2s;
+}
+
+.animar{
+  transition: opacity 2s;
+}
+.animou{
+  opacity: 0;
+}
+</style>
