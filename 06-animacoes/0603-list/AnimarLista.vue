206d3513d945dc98f44eceb103d14d07501bcc49
William R <wiribasczky@gmail.com>
2021-05-15 17:59:42 -0300

    Aula 0603 list

---
+++ 06-animacoes/0603-list/AnimarLista.vue
@@ -0,0 +1,54 @@
+<template>
+
+<div>
+  <button @click="adicionar">Adicionar</button>
+  <transition-group mode="out-in" tag="ul">
+    <li v-for="(produto,index) in carrinho" :key="produto.nome+index">
+      <p>{{ produto.nome }} - {{ produto.preco }}</p>
+      <button @click="remover(index)">Remover</button>
+    </li>
+  </transition-group>
+</div>
+  
+</template>
+<script>
+export default {
+  name: 'RemoverLista',
+  data() {
+    return {
+      carrinho: [
+        {
+          nome: 'Notebook',
+          preco: 2999
+        },
+        {
+          nome: 'Smartphone',
+          preco: 1399
+        }
+      ]
+    }
+  },
+  methods: {
+    remover(index) {
+      this.carrinho.splice(index, 1);
+    },
+    adicionar(){
+      this.carrinho.push({
+        nome:`Aleatorio ${this.carrinho.length}`,
+        preco: 1999
+      })
+    }
+  }
+}
+</script>
+<style>
+.v-enter,.v-leave-to{
+  opacity: 0;
+  transform: translate3d(-30px,0,0);
+}
+.v-enter-active,.v-leave-active{
+  transition: all 0.3s;
+}
+
+
+</style>
---
+++ 06-animacoes/0603-list/AnimarMoverLista.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <button @click="filtrarAZ">A-Z</button>
+    <button @click="filtrarZA">Z-A</button>
+    <transition-group tag="ul">
+      <li v-for="filme in filmes" :key="filme">{{filme}}</li>
+    </transition-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AnimarMoverLista",
+  data(){
+    return {
+      filmes: [
+        "O Senhor dos Aneis",
+        "As Crônicas de Nárnia",
+        "Coração Valente",
+        "Harry Potter"
+      ]
+    }
+  },
+  methods:{
+    filtrarAZ(){
+      this.filmes.sort();
+    },
+    filtrarZA(){
+      this.filmes.sort().reverse();
+    }
+  }
+}
+</script>
+
+<style scoped>
+.v-move{
+  transition: transform 0.3s;
+}
+ul{
+  list-style: none;
+  padding: 0;
+}
+li{
+  box-shadow: 0 2px 4px 0 rgba(0,0,0,0.1);
+  background: #fff;
+  padding: 20px;
+  max-width: 200px;
+  margin-bottom: 10px;
+}
+</style>
