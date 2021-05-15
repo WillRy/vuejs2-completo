0d4768acd83cc7ab1ba4d19f6bfe401371c1b79c
William R <wiribasczky@gmail.com>
2021-05-15 17:57:34 -0300

    Aula 0504 vue-cli

---
+++ 05-componentes/0504-vue-cli/vue-single-component/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{mensagem}}
+  </div>
+</template>
+<script>
+export default {
+  name: 'App',
+  data(){
+    return {
+      mensagem: 'Mensagem'
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
