6dab687318f295a955cae15dc53927cca165bac4
William R <wiribasczky@gmail.com>
2021-05-15 17:59:35 -0300

    Aula 0602 components

---
+++ 06-animacoes/0602-components/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li><a href="#" @click.prevent="menuAtivo = 'PaginaSobre'">Sobre</a></li>
+        <li><a href="#" @click.prevent="menuAtivo = 'PaginaServicos'">Serviços</a></li>
+      </ul>
+    </nav>
+    <hr>
+    <transition mode="out-in">
+      <keep-alive>
+        <component :is="menuAtivo"></component>
+      </keep-alive>
+    </transition>
+  </div>
+</template>
+
+<script>
+import PaginaSobre from "./PaginaSobre";
+import PaginaServicos from "./PaginaServicos";
+
+export default {
+  name: "Home",
+  components: {
+    PaginaSobre,
+    PaginaServicos
+  },
+  data() {
+    return {
+      menuAtivo: 'PaginaSobre'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.v-enter, .v-leave-to {
+  opacity: 0;
+  transform: translate3d(0,-20px,0);
+}
+
+.v-enter-active, .v-leave-active {
+  transition: all .3s;
+}
+</style>
---
+++ 06-animacoes/0602-components/PaginaServicos.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <h1>Serviços da empresa</h1>
+  <ul>
+    <li>FrontEnd</li>
+    <li>BackEnd</li>
+    <li>UX/UI Design</li>
+    <li>Marketing</li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+  name: "PaginaServicos"
+}
+</script>
+
+<style scoped>
+
+</style>
